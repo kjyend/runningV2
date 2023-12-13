f6bb1d419aa4f155aa0d557aef5824c859db5772
junyoung <adad568@naver.com>
2023-12-13 15:06:54 +0900

    Init : init project

---
+++ src/main/java/com/example/runningv2/RunningV2Application.java
@@ -0,0 +1,13 @@
+package com.example.runningv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/runningv2/RunningV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runningv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
